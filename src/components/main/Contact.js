9f0576d42f5de4360be68dbba52be76840f8ac65
ateliereven <orly.ev@gmail.com>
2022-02-26 12:48:37 +0200

    Add final form, fix validation issue to contact form

--- src/components/main/Contact.js
+++ src/components/main/Contact.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
+import { Field, Form } from 'react-final-form';
 import { init } from '@emailjs/browser';
 import { send } from 'emailjs-com';
 import { Typography, Container, Button, Box, Grid, TextField, InputAdornment } from "@mui/material";
@@ -6,67 +7,83 @@ import AccountCircle from '@mui/icons-material/AccountCircle';
 import EmailIcon from '@mui/icons-material/Email';
 import ChatIcon from '@mui/icons-material/Chat';
 import SendIcon from '@mui/icons-material/Send';
+import Slide from '@mui/material/Slide';
 
 
 export default function Contact() {
-
-    const defaultInputValues = {
-        name: '',
-        email: '',
-        message: ''
-    };
-    const [formInputs, setFormInputs] = useState(defaultInputValues);
     const [submitted, setSubmitted] = useState(false);
-    const [error, setError] = useState(false);
-    let missingInputError;
-    let emailError;
+    const containerRef = useRef(null);
 
-
-    const handleChange = e => {
-        setFormInputs({ ...formInputs, [e.target.id]: e.target.value });
+    const FIELDS = [
+        { label: 'Name', name: 'name', icon: <AccountCircle />, placeholder: 'Please enter your name' },
+        { label: 'Email', name: 'email', icon: <EmailIcon />, placeholder: 'Please enter your email address' },
+        { label: 'Message', name: 'message', icon: <ChatIcon />, placeholder: 'Drop me a message :)' },
+    ];
+    // create form fields:
+    const renderFields = (fields) => {
+        return fields.map((field) => {
+            return (
+                <Field
+                    fullWidth
+                    required
+                    name={field.name}
+                    component={renderInput}
+                    label={field.label}
+                    key={field.label}
+                    icon={field.icon}
+                    placeholder={field.placeholder}
+                />
+            )
+        })
     }
+    //Return an input element to the component prop of Field, and hook it with relevant properties deconstructed from formProps:  
+    const renderInput = ({ input, label, meta, icon, placeholder }) => {
+        return (
+            <TextField
+                label={label}
+                name={input.name}
+                value={input.value}
+                onChange={input.onChange}
+                error={meta.error && meta.touched ? true : false} // style as error
+                helperText={meta.error && meta.touched ? `${meta.error}` : ''} // display an error message
+                placeholder={placeholder}
+                size={input.name !== 'message' ? "small" : ""}
+                multiline={input.name !== 'message' ? false : true}
+                maxRows={6}
+                InputProps={{
+                    startAdornment: (
+                        <InputAdornment position="start">
+                            {icon}
+                        </InputAdornment>
+                    ),
+                }}
+            />
+        );
+    }
+
+    const onSubmit = (formValues) => {
+        // send via emailjs:
+        init(process.env.REACT_APP_EMAILJS_USER_ID);
+        send(
+            process.env.REACT_APP_EMAILJS_SERVICE_ID,
+            process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+            formValues,
+            process.env.REACT_APP_EMAILJS_USER_ID
+        )
+            .then((response) => {
+                console.log('SUCCESS!', response.status, response.text);
+                setSubmitted(true);
+            })
+            .catch((err) => {
+                console.log('FAILED...', err);
+            });
+    }
+
 
     // email validation:
     const validateEmails = (email) => {
         const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         if (re.test(email) === false) return 'Please enter a valid email address';
-    }
-    //form validation
-    const isValid = () => {
-        if (!formInputs.name || !formInputs.message) {
-            missingInputError = "This field is required";
-        }
-        //if no emails have been entered provide an empty string. if there are emails run the function:
-        emailError = validateEmails(formInputs.email || '');
-        if (missingInputError || emailError) {
-            setError(true);
-            console.log(emailError)
-            return false;
-        } else return true;
-    }
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if (isValid()) {
-            init(process.env.REACT_APP_EMAILJS_USER_ID);
-            send(
-                process.env.REACT_APP_EMAILJS_SERVICE_ID,
-                process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
-                formInputs,
-                process.env.REACT_APP_EMAILJS_USER_ID
-            )
-                .then((response) => {
-                    console.log('SUCCESS!', response.status, response.text);
-                    setSubmitted(true);
-                    setFormInputs(defaultInputValues);
-                    setError(false);
-                    emailError = '';
-                    missingInputError = '';
-                })
-                .catch((err) => {
-                    console.log('FAILED...', err);
-                });
-        }
     }
 
     return (
@@ -80,91 +97,56 @@ export default function Contact() {
                 </Typography>
                 <Container maxWidth="sm" sx={{ alignItems: 'center', justifyContent: 'center' }}>
                     {!submitted ?
-                        <Box
-                            component="form"
-                            autoComplete="off"
-                            noValidate
-                            onSubmit={handleSubmit}
-                            sx={{
-                                '& .MuiOutlinedInput-root': { borderRadius: '15px', backgroundColor: 'secondary.light' },
-                                '& .MuiInputLabel-root': { color: 'black', top: '-10px' },
-                                '& .MuiTextField-root': { m: 2.5, minWidth: '85%' }
+                        <Form
+                            onSubmit={onSubmit}
+                            validate={(formValues) => {
+                                const errors = {};
+                                //if no emails have been entered provide an empty string. if there are emails run the function:
+                                errors.email = validateEmails(formValues.email || '');
+                                if (!formValues.name) errors.name = "Name is required";
+                                if (!formValues.message) errors.message = "Email is required";
+                                return errors;
                             }}
-                        >
-                            <Grid container >
-                                <Grid item xs={12}>
-                                    <TextField
-                                        required
-                                        id="name"
-                                        label="Name"
-                                        value={formInputs.name}
-                                        onChange={e => handleChange(e)}
-                                        placeholder="Please enter name"
-                                        size="small"
-                                        InputProps={{
-                                            startAdornment: (
-                                                <InputAdornment position="start">
-                                                    <AccountCircle />
-                                                </InputAdornment>
-                                            ),
-                                        }}
-                                        error={error ? true : false}
-                                        helperText={missingInputError}
-                                    />
-                                    <TextField
-                                        required
-                                        id="email"
-                                        label="Email"
-                                        value={formInputs.email}
-                                        onChange={e => handleChange(e)}
-                                        placeholder="Please enter email address"
-
-                                        size="small"
-                                        type="email"
-                                        InputProps={{
-                                            startAdornment: (
-                                                <InputAdornment position="start">
-                                                    <EmailIcon />
-                                                </InputAdornment>
-                                            ),
-                                        }}
-                                        error={error ? true : false}
-                                        helperText={emailError}
-                                    />
-
-                                    <TextField
-                                        required
-                                        id="message"
-                                        label="Message"
-                                        value={formInputs.message}
-                                        onChange={e => handleChange(e)}
-                                        placeholder="Write me a message :)"
-                                        multiline
-                                        maxRows={6}
-                                        InputProps={{
-                                            startAdornment: (
-                                                <InputAdornment position="start">
-                                                    <ChatIcon />
-                                                </InputAdornment>
-                                            ),
-                                        }}
-                                        error={error ? true : false}
-                                        helperText={missingInputError}
-                                    />
-                                </Grid>
-                                <Grid container item sx={{ justifyContent: 'center' }} xs={11}>
-                                    <Button variant="contained" color="info" type="submit" value="Send" endIcon={<SendIcon />} >
-                                        Send
-                                    </Button>
-                                </Grid>
-                            </Grid>
-                        </Box>
-                        : <Typography variant="h4" sx={{ m: 5 }}>
-                            Thanks for getting in touch!
-                        </Typography>
+                            render={({ handleSubmit, submitting }) => (
+                                <Box
+                                    component="form"
+                                    autoComplete="off"
+                                    noValidate
+                                    onSubmit={handleSubmit}
+                                    sx={{
+                                        '& .MuiOutlinedInput-root': { borderRadius: '15px', backgroundColor: 'secondary.light' },
+                                        '& .MuiInputLabel-root': { color: 'black', top: '-10px' },
+                                        '& .MuiTextField-root': { m: 2.5, minWidth: '85%' }
+                                    }}
+                                >
+                                    <Grid container >
+                                        <Grid item xs={12}>
+                                            {renderFields(FIELDS)}
+                                        </Grid>
+                                        <Grid container item sx={{ justifyContent: 'center' }} xs={11}>
+                                            <Button
+                                                variant="contained"
+                                                color="info"
+                                                type="submit"
+                                                value="Send"
+                                                endIcon={<SendIcon />}
+                                                disabled={submitting}>
+                                                Send
+                                            </Button>
+                                        </Grid>
+                                    </Grid>
+                                </Box>
+                            )}
+                        />
+                        : <Slide direction="up" in={submitted} mountOnEnter unmountOnExit container={containerRef.current}>
+                            <Typography variant="h4" sx={{mt: 4}} >
+                                Thanks for getting in touch!
+                            </Typography>
+                        </Slide>
                     }
                 </Container>
             </Container>
         </Box>
     )
+
 }
